7b18a0e533f98bf711e10a1bf9c15cd22bc0430c
iron 3 <3iron38@gmail.com>
2022-02-04 11:40:25 +0900

    4344

--- 4344.py
+++ 4344.py
@@ -5,5 +5,16 @@ for i in range(c):
     n = list(map(int, input().split()))
 
     sum = 0
-    for i in range(n[0]):
-        sum += n[i + 1]
+    for i in range(n[0]):  # n = [5, 50, 70, 80, 100]
+        sum += n[i + 1]    # sum = 350
+
+    cnt = 0                # 평균 이상 학생들
+    for i in range(n[0]):  # sum / n[0] = 70
+        if n[i + 1] > (sum / n[0]):
+            cnt += 1
+
+    avg.append((cnt / n[0]) * 100)
+    n.clear()       # 입력받는 n 리스트 초기화
+
+for i in range(c):
+    print("%.3f" %avg[i] + "%")
