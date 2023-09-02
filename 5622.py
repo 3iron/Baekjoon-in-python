e2f7a8e7b4acb12c209faf85117b8d97be1cd9ee
3iron <82188346+3iron@users.noreply.github.com>
2023-09-02 08:34:44 +0000

    다이얼 - 각 알파벳의 초 검색 완료

--- 5622.py
+++ 5622.py
@@ -7,17 +7,18 @@ dial = ['abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
 
 for i in range(len(dial)):       # len(dial) = 8, i=0 일 때 dial[0]은 'abc'
     p = dial[i]
-    for j in range(len(p)-1):      # len(p) = 3,  j=0 일 때 p[0:1] 은 a
-        r = 1
-        print(p[j:r])
+    r = 0
+    for j in range(len(p)):      # len(p) = 3,  j=0 일 때 p[0:1] 은 a
+        r = r + 1
+        # print(p[j:r])
+        
         if a == p[j:r] :
-            print(p[j:r])
-            print(j)
-            print(r)
-            print(i+1)
+            # print(p[j:r])
+            # print(j)
+            # print(r)
+            print(i+2)
             # answer = i+1
-        else :
-            r += 1
+            
         
         
         # if a == p[j:r] :
