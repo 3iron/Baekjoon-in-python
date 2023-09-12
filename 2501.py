99b60d6495a3ad65e4e2d0b5fd36750894d09b8b
3iron <82188346+3iron@users.noreply.github.com>
2023-09-12 13:39:40 +0000

    약수 구하기

---
+++ 2501.py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+arr = []
+
+for i in range(1, n+1):
+    #print(i)
+    if (n%i == 0) :
+        arr.append(i)
+
+if k > len(arr):
+    print(0)
+else:
+    print(arr[k-1])
+
