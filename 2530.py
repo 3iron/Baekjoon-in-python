6983984289974427deafda9268cedec539e01d95
3iron <82188346+3iron@users.noreply.github.com>
2023-09-17 13:11:40 +0000

    정답

--- 2530.py
+++ 2530.py
@@ -1,22 +1,8 @@
 a, b, c = map(int, input().split())
-d = int(input())
-
-if(d//60>0):
-    b += d//60
-    c += (d - d//60 * 60)
-
-if (c//60>0):
-    b += (c//60)
-    c -= (c//60) * 60
-
-if (b//60>0):
-    a += (b//60)
-    b -= (b//60) * 60
-    
-if (a//24>0):
-    a -= (a//24) * 24 
-    
+c = c + int(input())
+b = b + int(c//60)
+a = a + int(b//60)
+a = a % 24
+b = b % 60
+c = c % 60
 print(a, b, c)
-
-
-
