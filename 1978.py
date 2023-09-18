4a87cf2521a287f5ad523013c25b9dcbbe93aa64
3iron <82188346+3iron@users.noreply.github.com>
2023-09-18 14:35:51 +0000

    소수 찾기 진행중

---
+++ 1978.py
@@ -0,0 +1,22 @@
+n = int(input())
+arr = list(map(int, input().split()))
+cnt = 0
+t = 0
+
+for i in range(0, len(arr)+1):
+    # print(arr[i]) # 1 3 5 7
+    for t in range(1, arr[i]+1):
+        #if (arr[i]//t > 0):  # i = 0 일 때 , 1//1 > 0
+        #    cnt += 1
+        if (arr[i]==1):
+            i+=1
+        elif (arr[i]%t == 0):
+                cnt +=1 
+        print(cnt)
+     
+    
+#    if (arr[i]//i == 1):
+#        print(arr[i])
+
+
+# i = 1 일 때, arr[1]//2 =
