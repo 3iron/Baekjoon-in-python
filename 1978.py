8961ed458a4e5b2d9b48a82f455d2c5f93cb2650
3iron <82188346+3iron@users.noreply.github.com>
2023-09-21 14:04:06 +0000

    다른 사람 풀이 추가

--- 1978.py
+++ 1978.py
@@ -1,18 +1,33 @@
-n = int(input())
-arr = list(map(int, input().split()))
-t = 0
-# answer = 0
+# n = int(input())
+# arr = list(map(int, input().split()))
+# t = 0
+# # answer = 0
 
 
-for i in range(0, len(arr)+1):
-    cnt = 0
-    for t in range(1, arr[i]+1):
-        if (arr[i]%t == 0):
-            # print(arr[i], t) # i=0, t = 1 -> 1, 1.  // i=1, t=1, t=3 
-            cnt +=1
-        if cnt == 2:
-            print(arr[i], " 가 소수에요")
-            # answer += 1
-
-# print(answer)
+# for i in range(0, len(arr)+1):
+#     cnt = 0
+#     for t in range(1, arr[i]+1):
+#         if (arr[i]%t == 0):
+#             # print(arr[i], t) # i=0, t = 1 -> 1, 1.  // i=1, t=1, t=3 
+#             cnt +=1
+#         if cnt == 2:
+#             print(arr[i], " 가 소수에요")
+#             # answer += 1
+ 
+# # print(answer)
         
+# 다른 사람 풀이
+
+n = int(input())
+data = list(map(int, input().split()))
+count = 0
+
+for x in data:
+  for i in range(2, x+1):
+    if x % i == 0:
+      if x == i:
+        count += 1
+      
+      break
+
+print(count)
