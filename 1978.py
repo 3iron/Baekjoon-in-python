d20170ef4775193d145ba06dd71c54b9da173571
3iron <82188346+3iron@users.noreply.github.com>
2023-09-23 12:46:45 +0000

    해결 완

--- 1978.py
+++ 1978.py
@@ -1,61 +1,17 @@
-# n = int(input())
-# arr = list(map(int, input().split()))
-# t = 0
-# # answer = 0
-
-
-# for i in range(0, len(arr)+1):
-#     cnt = 0
-#     for t in range(1, arr[i]+1):
-#         if (arr[i]%t == 0):
-#             # print(arr[i], t) # i=0, t = 1 -> 1, 1.  // i=1, t=1, t=3 
-#             cnt +=1
-#         if cnt == 2:
-#             print(arr[i], " 가 소수에요")
-#             # answer += 1
- 
-# # print(answer)
-        
-# 다른 사람 풀이
-
 n = int(input())
-data = list(map(int, input().split()))
-count = 0
+arr = list(map(int, input().split()))
+a = 0
+answer = 0
 
-for x in data:
-  for i in range(2, x+1):
-    if x % i == 0:
-      if x == i:
-        count += 1
-      
-      break
+for i in range(0, n): 
+    a = arr[i]
+    cnt = 0
 
-print(count)
-
-
-# # 다시 차근차근 풀기
-# n = int(input())
-# arr = list(map(int, input().split()))
-# t = 0
-# answer = 0
-
-
-# for i in range(0, len(arr)):
-#     print("i",i)
-#     # print("len(arr)=",len(arr))
-#     answer+=1 
-#     # print(answer)
-#     print(arr[i])
+    for _ in range(1, a+1):
+        if(a%_==0):        
+            cnt += 1 
+    if (cnt == 2):
+        answer += 1
     
-#     # cnt = 0
-#     # for t in range(1, len(arr[i])-1):
-#     #     if (arr[i]%t == 0):
-#     #         # print(arr[i], t) 
-#     #         cnt +=1
-#     # if cnt == 2:
-#     #     print(arr[i], " 가 소수에요")
-#     #     #answer += 1
-#     #     break
- 
-# #print(answer)
-        
+
+print(answer)
